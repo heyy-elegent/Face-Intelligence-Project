015f5b16b276b349a65af27bec44b7a9a3847af6
l4kushwaha <221704379+elegent786@users.noreply.github.com>
2026-02-25 09:36:46 +0530

    Initial Clean Commit: Code Only

---
+++ app.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+# age_gender_model = tf.keras.models.load_model("age_gender_model.h5")
+emotion_model = tf.keras.models.load_model("emotion_vgg19_final.h55")
+
+print("Models Loaded Successfully ✅")
